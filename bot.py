83a17c668ebda6abba76c4dd3e0bff0db82757f7
Maarij Ali <maarijali29@gmail.com>
2025-05-25 01:37:16 +0500

    Add all files inside the frontend folder

--- bot.py
+++ bot.py
@@ -199,8 +199,5 @@ def handle_prompt():
 #         return send_from_directory('../frontend/build', 'index.html')
 
 if __name__ == '__main__':
-   
- port = int(os.environ.get('PORT', 8000))
-   
-    
+port = int(os.environ.get('PORT', 8000))
 app.run(host='0.0.0.0', port=port, debug=False)
